bee524b277b3d47fb80129b096f8e23b070267ca
KasTheDev <118139996+KasTheDev@users.noreply.github.com>
2023-04-01 17:58:57 -0700

    Initial Commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        image: "images/pizza.jpg",
+        id: 0,
+        quantity: 0
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "images/burger-2.jpg",
+        id: 1,
+        quantity: 0
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        image: "images/beer-2.png",
+        id: 2,
+        quantity: 0
+    }
+]
---
+++ index.js
@@ -0,0 +1,92 @@
+import { menuArray } from './data.js'
+
+const orderItems = document.getElementById('order-items')
+const orderContainer = document.getElementById('order-container')
+const totalPrice = document.getElementById('total-price')
+const completeBtn = document.getElementById('complete-btn')
+const modal = document.getElementById('modal')
+const payBtn = document.getElementById('pay-btn')
+const outgoingMsg = document.getElementById('outgoing-msg')
+
+//--Menu--//
+
+function getMenuHtml(){
+    let menuHtml=``
+    menuArray.forEach(function(menu){
+        menuHtml +=`
+    <div class="menu-items">
+            <img src= "${menu.image}" class="icon-img">
+                <div class="item-container">
+                     <h2>${menu.name}</h2>
+                    <p1>${menu.ingredients}</p1>
+                    <p2>$${menu.price}</p2>
+                </div>
+                <button class="add-btn" data-add= "${menu.id}">+</button>
+    </div>`
+    })
+    return menuHtml
+}
+
+function render(){
+    document.getElementById('menu-container').innerHTML = getMenuHtml()
+}
+
+render()
+
+//--Order--//
+
+document.addEventListener('click', function(e){
+     if(e.target.dataset.add) {
+        addOrderClick(e.target.dataset.add)
+        }
+    else if(e.target.dataset.remove){
+        removeOrderClick(e.target.dataset.remove)
+        }
+    else if(e.target.dataset.id == 'complete-btn'){
+        modal.style.display = 'flex';
+    }
+    else if(e.target.id == 'pay-btn'){
+        outgoingMsg.style.display = 'block'
+    }
+})
+
+function addOrderClick(itemId) {
+    menuArray[itemId].quantity += 1;
+    renderOrder()
+}
+
+function removeOrderClick(itemId){
+    menuArray[itemId].quantity -= 1;
+    renderOrder()
+}
+
+function renderOrder(){
+    let orderHtml=``
+    let totalCost = 0;
+    orderItems.innerHTML = ``;
+    menuArray.forEach((item) => {
+        if(item.quantity > 0) {
+            totalCost += item.price * item.quantity
+            orderItems.innerHTML += 
+                `
+                    <div class="order-item">
+                        <div class="order-item-info">
+                            <p>${item.quantity}</p>
+                            <h3>${item.name}</h3>
+                            <button class="remove-btn" data-remove="${item.id}">remove</button>
+                        </div>
+                        <h3 class="item-price">$${item.quantity * item.price}</p>
+                    </div>  
+                `
+        }
+        if(totalCost == 0) {
+            orderContainer.style.display = 'none';
+        } 
+        else {
+            orderContainer.style.display = 'block';
+            totalPrice.innerHTML = `$${totalCost}`;
+        }
+    })
+}
+console.log(payBtn)
+console.log('Event listener added.')
